0c5dd49b67bf72e3172cd42ce6b810690f40614b
Tanguy <tanguy.guillo@gmail.com>
2022-06-14 14:37:58 +0200

    changing CSS in css directory

--- public/index.php
+++ public/index.php
@@ -2,30 +2,37 @@
 <html class="no-js" lang="fr">
 
 <head>
-  <meta charset="utf-8">
-  <title>Blog de OmegaWebProd</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<title>Blog de OmegaWebProd</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
+<meta property="og:title" content="">
+<meta property="og:type" content="">
+<meta property="og:url" content="">
+<meta property="og:image" content="">
 
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
+<link rel="manifest" href="site.webmanifest">
+<link rel="apple-touch-icon" href="icon.png">
+<!-- Place favicon.ico in the root directory -->
 
-      <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap Core CSS -->
+<!--<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet"> -->
+<link href="css/styles.css" rel="stylesheet"> 
+
+    <!-- CSS only -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">-->
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
     <!-- Theme CSS -->
     <link href="css/freelancer.min.css" rel="stylesheet">
 
-     <!-- Theme CSS blog.omegawebprod -->
+<!-- Theme CSS blog.omegawebprod -->
     <link href="css/blog-omegawebprod.css" rel="stylesheet">
 
-       <!-- Custom Fonts -->
+<!-- Custom Fonts -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
@@ -39,16 +46,16 @@
 
 
 
-  <!--<link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">-->
+<!--<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/main.css">-->
 
-  <meta name="theme-color" content="#fafafa">
+<meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
 
-  <!-- Add your site or application content here -->
-  <span id = "home"></span>
+<!-- Add your site or application content here -->
+<span id = "home"></span>
 
 <!-- Navigation -->
     <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
@@ -73,7 +80,7 @@
                     <li class="page-scroll">
                         <a href="#">Blog</a>
                     </li>
-                     <li class="page-scroll">
+                    <li class="page-scroll">
                         <a href="#contact">Contact</a>
                     </li>
                     <li class="page-scroll">
@@ -87,7 +94,7 @@
     </nav>
 
 
- <!-- Header -->
+<!-- Header -->
     <header>
         <div class="container">
             <div class="row">
@@ -108,7 +115,7 @@
 
 
 
-   <!-- Contact Section -->
+<!-- Contact Section -->
     <section id="contact">
         <div class="container">
             <div class="row">
@@ -163,18 +170,14 @@
         </div>
     </section>
 
-
-
-
-
-
- <!-- Footer -->
+    
+<!-- Footer -->
     <footer class="text-center">
         <div class="footer-above">
             <div class="container">
                 <div class="row">
                     <div class="footer-col col-md-4">
-                     <h3>Nous trouvez</h3>
+                    <h3>Nous trouvez</h3>
                         <p>109 rue de Fleury
                         <br>92140 Clamart
                         <br>France
@@ -207,13 +210,13 @@
                     </div>
                     <div class="footer-col col-md-4">
                     <div class="" id = "about">
-                       <h3>A propos de OmegaWebProd</h3>
+                    <h3>A propos de OmegaWebProd</h3>
                         <p>Nous sommes spécialisé dans la réalisation de sites web "sur mesure" pour de petites structures</p>
                         <!-- <p>In his tractibus navigerum nusquam visitur flumen sed in locis plurimis aquae suapte natura calentes emergunt ad
                         usus aptae multiplicium medelarum. verum has quoque regiones pari sorte Pompeius Iudaeis domitis
                         et Hierosolymis captis in provinciae speciem delata iuris dictione formavit.-->
                         </p>
-                      </div>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -224,15 +227,13 @@
                     <span class="col-lg-6">
                         OmegaWebProd Copyright &copy; 2022
                     </span>
-                     </span class="col-lg-6">
-                      <a href="#confidential" class="portfolio-link" data-toggle="modal">Politique de confidentialité</a> | <a href="#">Admin</a>
-                     </span>
+                    </span class="col-lg-6">
+                    <a href="#confidential" class="portfolio-link" data-toggle="modal">Politique de confidentialité</a> | <a href="#">Administrationn</a>
+                    </span>
                 </div>
             </div>
         </div>
     </footer>
-
-
 
     <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
     <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
@@ -240,7 +241,6 @@
             <i class="fa fa-chevron-up"></i>
         </a>
     </div>
-
 
 <!-- modal-->
     <div class="portfolio-modal modal fade" id="confidential" tabindex="-1" role="dialog" aria-hidden="true">
@@ -255,33 +255,37 @@
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2">
                         <div class="modal-body">
-                           <h1> Qui sommes-nous ? </h1>
-                          <p>L’adresse de notre site est : https://blog.omegawebblog.com</p>
-                          <h2>commentaires</h2>
-<p>Quand vous laissez un commentaire sur notre site, les données inscrites dans le formulaire de commentaire, ainsi que votre adresse IP et l’agent utilisateur de votre navigateur sont collectés pour nous aider à la détection des commentaires indésirables.
-Une chaîne anonymisée créée à partir de votre adresse e-mail (également appelée hash) peut être envoyée au service Gravatar pour vérifier si vous utilisez ce dernier. Les clauses de confidentialité du service Gravatar sont disponibles ici : https://automattic.com/privacy/. Après validation de votre commentaire, votre photo de profil sera visible publiquement à coté de votre commentaire.</p>
-Médias
+                        <h1> Qui sommes-nous ? </h1>
+                        <p>L’adresse de notre blog est : https://blog.omegawebblog.com</p>
+                        <p>L’adresse de notre site est : https://omegawebblog.com</p>
+                        <h2>commentaires</h2>
+<p>Quand vous laissez un commentaire sur notre blog, les données inscrites dans le formulaire de commentaire, ainsi que votre adresse IP et l’agent utilisateur de votre navigateur sont collectés pour nous aider à la détection des commentaires indésirables.
 Si vous téléversez des images sur le site, nous vous conseillons d’éviter de téléverser des images contenant des données EXIF de coordonnées GPS. Les personnes visitant votre site peuvent télécharger et extraire des données de localisation depuis ces images.
-Cookies
-Si vous déposez un commentaire sur notre site, il vous sera proposé d’enregistrer votre nom, adresse e-mail et site dans des cookies. C’est uniquement pour votre confort afin de ne pas avoir à saisir ces informations si vous déposez un autre commentaire plus tard. Ces cookies expirent au bout d’un an.
+Cookies</p>
+
+<p> vous déposez un commentaire sur notre site, il vous sera proposé d’enregistrer votre nom, adresse e-mail et site dans des cookies. 
+    <br />C’est uniquement pour votre confort afin de ne pas avoir à saisir ces informations si vous déposez un autre commentaire plus tard. Ces cookies expirent au bout d’un an.
 Si vous vous rendez sur la page de connexion, un cookie temporaire sera créé afin de déterminer si votre navigateur accepte les cookies. Il ne contient pas de données personnelles et sera supprimé automatiquement à la fermeture de votre navigateur.
-Lorsque vous vous connecterez, nous mettrons en place un certain nombre de cookies pour enregistrer vos informations de connexion et vos préférences d’écran. La durée de vie d’un cookie de connexion est de deux jours, celle d’un cookie d’option d’écran est d’un an. Si vous cochez « Se souvenir de moi », votre cookie de connexion sera conservé pendant deux semaines. Si vous vous déconnectez de votre compte, le cookie de connexion sera effacé.
+<br />Lorsque vous vous connecterez, nous mettrons en place un certain nombre de cookies pour enregistrer vos informations de connexion et vos préférences d’écran. La durée de vie d’un cookie de connexion est de deux jours, celle d’un cookie d’option d’écran est d’un an. Si vous cochez « Se souvenir de moi », votre cookie de connexion sera conservé pendant deux semaines. Si vous vous déconnectez de votre compte, le cookie de connexion sera effacé.
 En modifiant ou en publiant une publication, un cookie supplémentaire sera enregistré dans votre navigateur. Ce cookie ne comprend aucune donnée personnelle. Il indique simplement l’ID de la publication que vous venez de modifier. Il expire au bout d’un jour.
-Contenu embarqué depuis d’autres sites
-Les articles de ce site peuvent inclure des contenus intégrés (par exemple des vidéos, images, articles…). Le contenu intégré depuis d’autres sites se comporte de la même manière que si le visiteur se rendait sur cet autre site.
-Ces sites web pourraient collecter des données sur vous, utiliser des cookies, embarquer des outils de suivis tiers, suivre vos interactions avec ces contenus embarqués si vous disposez d’un compte connecté sur leur site web.
-Utilisation et transmission de vos données personnelles
-Si vous demandez une réinitialisation de votre mot de passe, votre adresse IP sera incluse dans l’e-mail de réinitialisation.
-Durées de stockage de vos données
-Si vous laissez un commentaire, le commentaire et ses métadonnées sont conservés indéfiniment. Cela permet de reconnaître et approuver automatiquement les commentaires suivants au lieu de les laisser dans la file de modération.
+Contenu embarqué depuis d’autres sites</p>
+
+<p>Les articles de ce site peuvent inclure des contenus intégrés (par exemple images, articles…). Le contenu intégré depuis d’autres sites se comporte de la même manière que si le visiteur se rendait sur cet autre site.
+Ces sites web pourraient collecter des données sur vous, utiliser des cookies, embarquer des outils de suivis tiers, suivre vos interactions avec ces contenus embarqués si vous disposez d’un compte connecté sur leur site web.</p>
+
+<h2>Utilisation et transmission de vos données personnelles</h2>
+
+<p>Si vous demandez une réinitialisation de votre mot de passe, votre adresse IP sera incluse dans l’e-mail de réinitialisation.<p/>
+
+<h2>Durées de stockage de vos données</h2>
+
+<p>Si vous laissez un commentaire, le commentaire et ses métadonnées sont conservés indéfiniment. Cela permet de reconnaître et approuver automatiquement les commentaires suivants au lieu de les laisser dans la file de modération.
 Pour les comptes qui s’inscrivent sur notre site (le cas échéant), nous stockons également les données personnelles indiquées dans leur profil. Tous les comptes peuvent voir, modifier ou supprimer leurs informations personnelles à tout moment (à l’exception de leur identifiant). Les gestionnaires du site peuvent aussi voir et modifier ces informations.
 Les droits que vous avez sur vos données
-Si vous avez un compte ou si vous avez laissé des commentaires sur le site, vous pouvez demander à recevoir un fichier contenant toutes les données personnelles que nous possédons à votre sujet, incluant celles que vous nous avez fournies. Vous pouvez également demander la suppression des données personnelles vous concernant. Cela ne prend pas en compte les données stockées à des fins administratives, légales ou pour des raisons de sécurité.
-Transmission de vos données personnelles
-Les commentaires des visiteurs peuvent être vérifiés à l’aide d’un service automatisé de détection des commentaires indésirables.
+Si vous avez un compte ou si vous avez laissé des commentaires sur le site, vous pouvez demander à recevoir un fichier contenant toutes les données personnelles que nous possédons à votre sujet, incluant celles que vous nous avez fournies. Vous pouvez également demander la suppression des données personnelles vous concernant. Cela ne prend pas en compte les données stockées à des fins administratives, légales ou pour des raisons de sécurité.<p>
 
-
-
+<h2>Transmission de vos données personnelles</h2>
+<p>Les commentaires des visiteurs peuvent être vérifiés à l’aide d’un service automatisé de détection des commentaires indésirables.</p>
 
                             <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
                         </div>
@@ -291,12 +295,11 @@ Les commentaires des visiteurs peuvent être vérifiés à l’aide d’un servi
         </div>
     </div>
 
+<!-- <script src="js/vendor/modernizr-3.11.2.min.js"></script>-->
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
 
-  <!-- <script src="js/vendor/modernizr-3.11.2.min.js"></script>-->
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-
-   <!-- jQuery -->
+<!-- jQuery -->
     <script src="vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
@@ -309,15 +312,15 @@ Les commentaires des visiteurs peuvent être vérifiés à l’aide d’un servi
     <script src="js/jqBootstrapValidation.js"></script>
     <script src="js/contact_me.js"></script>
 
-  <!-- Theme JavaScript -->
+<!-- Theme JavaScript -->
     <script src="js/freelancer.min.js"></script>
 
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
     window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async></script>
 </body>
 
 </html>
