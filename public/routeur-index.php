ebd9e6bb1caee17da3b0d9a8b1436c10e0f16415
Tanguy <tanguy.guillo@gmail.com>
2022-06-22 05:08:35 +0200

    adding routeur-index.php

---
+++ public/routeur-index.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('src/controllers/add_comment.php');
+require_once('src/controllers/homepage.php');
+require_once('src/controllers/post.php');
+
+use Application\Controllers\AddComment\AddComment;
+use Application\Controllers\Homepage\Homepage;
+use Application\Controllers\Post\Post;
+
+try {
+    if (isset($_GET['action']) && $_GET['action'] !== '') {
+        if ($_GET['action'] === 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+
+                (new Post())->execute($identifier);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } elseif ($_GET['action'] === 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+
+                (new AddComment())->execute($identifier, $_POST);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } else {
+            throw new Exception("La page que vous recherchez n'existe pas.");
+        }
+    } else {
+        (new Homepage())->execute();
+    }
+} catch (Exception $e) {
+    $errorMessage = $e->getMessage();
+
+    require('templates/error.php');
+}
