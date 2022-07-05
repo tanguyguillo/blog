53c9cfb75535258780c363e4a76a4c5265515d29
Tanguy <tanguy.guillo@gmail.com>
2022-07-05 10:49:42 +0200

    adding user with identifier working

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,45 +1,78 @@
 <?php
 
-namespace Application\Models\userModel;
+namespace Application\Models\UserModel;
 
-//use Application\Lib\Database\DatabaseConnection;
 use Application\Core\Database\Database\DatabaseConnection;
 
-class PostsModel
-{	
-public $id;
-public $EmailUser;
-public $passWordUser;
-public $nameUser;
-public $surNameUser;
-public $titleUser;
-public $telGsmUser;
-public $roleUser;
-public $pictureOrLogo;
+class UserModel
+{
+    public $id;
+    public $EmailUser;
+    public $passWordUser;
+    public $nameUser;
+    public $surNameUser;
+    public $titleUser;
+    public $telGsmUser;
+    public $roleUser;
+    public $pictureOrLogo;
 }
 
-class UsersRepository
+class User
 {
-    //public DatabaseConnection $connection;
 
     /**
+     * function to get a user with all of this properties
      *
-     * return an Array
+     * @param string $identifier
+     * @return Array
      */
-    public function getUsers(): //array
+    public function getUser(string $identifier): array
     {
-        // $statement = $this->connection->getConnection()->query(
-        //     "SELECT id, postTitle, postChapo, DATE_FORMAT(postModified, '%d/%m/%Y à %Hh%imin') AS french_modified_date FROM blog_post ORDER BY postModified DESC LIMIT 0, 5"
-        // );
-        // $posts = [];
-        // while (($row = $statement->fetch())) {
-        //     $post = new PostsRepository();
-        //     $post->postTitle = $row['postTitle'];
-        //     $post->frenchModifiedDate = $row['french_modified_date'];
-        //     $post->postChapo = $row['postChapo'];
-        //     $post->id = $row['id'];
-        //     $posts[] = $post;
-        // }
-        // return $posts;
+        $statement = $this->connection->getConnection()->query(
+            //"SELECT id; EmailUser; passWordUser; nameUser; surNameUser; titleUser; telGsmUser; roleUser; pictureOrLogo;  FROM user where id = $identifier"
+            "SELECT * FROM user where id = $identifier"
+        );
+
+        $statement->execute();
+
+        $row = $statement->fetch();
+        $user = new User();
+        $user->idUser = $row['id'];
+        $user->emailUser = $row['EmailUser'];
+        $user->passWordUser = $row['passWordUser'];
+        $user->nameUser = $row['nameUser'];
+        $user->surNameUser = $row['surNameUser'];
+        $user->titleUser = $row['titleUser'];
+        $user->telGsmUser = $row['telGsmUser'];
+        $user->roleUser = $row['roleUser'];
+        $user->pictureOrLogo = $row['pictureOrLogo'];
+
+        $UserArray[] = $user;
+
+        return $UserArray;
     }
+
+
+    // /**
+    //  *
+    //  * return an Array
+    //  */
+    // public function getUsers(): //array
+    // {
+    //     // $statement = $this->connection->getConnection()->query(
+    //     //     "SELECT id, postTitle, postChapo, DATE_FORMAT(postModified, '%d/%m/%Y à %Hh%imin') AS french_modified_date FROM blog_post ORDER BY postModified DESC LIMIT 0, 5"
+    //     // );
+    //     // $posts = [];
+    //     // while (($row = $statement->fetch())) {
+    //     //     $post = new PostsRepository();
+    //     //     $post->postTitle = $row['postTitle'];
+    //     //     $post->frenchModifiedDate = $row['french_modified_date'];
+    //     //     $post->postChapo = $row['postChapo'];
+    //     //     $post->id = $row['id'];
+    //     //     $posts[] = $post;
+    //     // }
+    //     // return $posts;
+    // }
+
+
 }
