d91b39e357f59cb564bbbc5220dc78e26a76dc57
Tanguy <tanguy.guillo@gmail.com>
2022-07-05 13:07:37 +0200

    adding show if "open" with twig

--- app/models/postsModel.php
+++ app/models/postsModel.php
@@ -19,8 +19,6 @@ class PostsModel
 
 class PostsRepository
 {
-    //public DatabaseConnection $connection;
-
     /**
      *
      * return an Array
@@ -28,11 +26,12 @@ class PostsRepository
     public function getPosts(): array
     {
         $statement = $this->connection->getConnection()->query(
-            "SELECT id, postTitle, postChapo, DATE_FORMAT(postModified, '%d/%m/%Y à %Hh%imin') AS french_modified_date FROM blog_post ORDER BY postModified DESC LIMIT 0, 5"
+            "SELECT id, postTitle, postChapo, postStatus, DATE_FORMAT(postModified, '%d/%m/%Y à %Hh%imin') AS french_modified_date FROM blog_post ORDER BY postModified DESC LIMIT 0, 5"
         );
         $posts = [];
         while (($row = $statement->fetch())) {
             $post = new PostsRepository();
+            $post->postStatus = $row['postStatus'];
             $post->postTitle = $row['postTitle'];
             $post->frenchModifiedDate = $row['french_modified_date'];
             $post->postChapo = $row['postChapo'];
