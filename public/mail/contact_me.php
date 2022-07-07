d33557f495ca61790c3cc67b38908ec15a8bb795
Tanguy <tanguy.guillo@gmail.com>
2022-07-07 07:51:44 +0200

    modify contact_me.php

--- public/mail/contact_me.php
+++ public/mail/contact_me.php
@@ -18,10 +18,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'blog-posts@omegawebprod.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@y@omegawebprod.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'blog.@omegawebprod.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulaire de contact de blog.@omegawebprod.com:  $name";
+$email_body = "Une nouvelle demande d'info.\n\n" . "les details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@blog.omegawebprod.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to, $email_subject, $email_body, $headers);
 
