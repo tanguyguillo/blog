ad57b6922dafc9e46635eb1727531021ef88d2b7
Tanguy <tanguy.guillo@gmail.com>
2022-08-12 12:17:31 +0200

    fixing issues for Pascal Case fot folder Model, Core, Database,Controllers which make the web site down on line

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Application\Controllers;
 
 use Application\Core\Auth\Auth;
--- app/myAutoloader.php
+++ app/myAutoloader.php
@@ -2,10 +2,10 @@
 
 /**
  *  function making working autoload using $class provided by composer
- * 
- * autododer of composer do for exemple : Application\Controllers\DetailController\DetailController (DetailController\DetailController : X2) 
- * 
- * function 
+ *
+ * autododer of composer do for exemple : Application\Controllers\DetailController\DetailController (DetailController\DetailController : X2)
+ *
+ * function
  *
  * @param [string] $class
  * @return void
@@ -17,7 +17,8 @@ function my_autoloader($class)
     $newarray = array_slice($myArray, 1, -1);
     $delim = "/";
     $class2 = implode($delim, $newarray);
-    $path = (ROOT . "/app/" . $class2 . ".php");
+
+    $path = (dirname(__DIR__)  . "/app/" . $class2 . ".php");
 
     if (file_exists($path)) {
         require $path;
